9cf3c8a6951dabf48ae096aee3d7cc110c17429e
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2023-05-11 11:31:06 +0600

    upgrade project

--- android/app/src/main/kotlin/com/example/upgrade_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/upgrade_project/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.chat
+package com.example.upgrade_project
 
 import io.flutter.embedding.android.FlutterActivity
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,13 @@
 // This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
+// utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:chat/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:chat/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
